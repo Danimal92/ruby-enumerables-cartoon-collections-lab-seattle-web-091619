a42ae530090c056b7b6d1c81defe2bc7e7e449dd
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-30 09:37:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,12 @@ array.each_with_index.map { |x,i|
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  
+array.map { |word|
+    "#{word.capitalize}!"}
+  
 end
 
 def long_planeteer_calls# code an argument here
